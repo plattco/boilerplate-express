4f06e3c71cf3f9193c968b6b372900d0aa062caf
Cody Platt <62489180+plattco@users.noreply.github.com>
2021-07-16 16:30:03 -0400

    updated

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 
-
+absolutePath = __dirname + relativePath /views/index.html
+app.get("/", function (req, res) {
+    res.sendFile(absolutePath);
+});
 
 
 
